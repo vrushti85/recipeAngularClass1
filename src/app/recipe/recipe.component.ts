08b258334fa64bc5ed973f7af117e3d6218a635f
manishpatidar028 <manish.patidar.sa@gmail.com>
2019-12-30 16:17:36 +0530

    initial commit

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComponent implements OnInit {
 
-  constructor() { }
+  foodsStall = [
+    {
+      recipeName: 'Burger',
+      chefName: 'Ranveer Brar',
+      imageUrl: 'http://salemdigest.com/wp-content/uploads/2016/08/TITS_food1.jpg',
+      type: 'non-vegetarian',
+      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+      recipeName: 'Italian Pasta',
+      chefName: 'JM',
+      imageUrl: 'https://mariettasquaremarket.com/msm/wp-content/uploads/2018/12/Pita-Mediterranean-5.jpg',
+      type: 'vegetarian',
+      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+      recipeName: 'Chicken Maggie',
+      chefName: 'Nisha Madhulika',
+      imageUrl: 'http://eatbook.sg/wp-content/uploads/2018/06/Century-Square-Food-Two-Hana.jpg',
+      type: 'non-vegetarian',
+      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+      recipeName: 'Veg. Pulav',
+      chefName: 'Tarla Dalal',
+      imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-p/0e/75/7b/5d/photo3jpg.jpg',
+      type: 'vegetarian',
+      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+  ];
+  public addRecipeForm: FormGroup;
+  constructor(
+    private formbuilder: FormBuilder,
+
+  ) { }
 
   ngOnInit() {
+    this.addRecipeForm = this.formbuilder.group(
+      {
+        recipeName: ['', Validators.required],
+        imageUrl: ['', Validators.required],
+        description: ['', Validators.required],
+        type: ['', Validators.required],
+        chefName: ['', Validators.required],
+      });
   }
 
+  get resetPass() { return this.addRecipeForm.controls; }
+
+  addrecipe() {
+    this.foodsStall.push(this.addRecipeForm.value);
+    console.log("this.addRecipeForm.value::", this.addRecipeForm.value)
+  }
 }
